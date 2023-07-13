01fb373777d0f26fab09cbe960c2fbc44a395101
Konstantinos Mixios <k.mixios@gmail.com>
2023-07-13 15:17:09 +0300

    Truss Element class

--- lesa/elements.py
+++ lesa/elements.py
@@ -62,3 +62,52 @@ class Bar(Element):
 
     def get_nodes(self):
         return self.nodes
+
+
+class Truss(Element):
+    def __init__(self, nodes, E, A):
+        super().__init__(self, element_type='truss')
+        self.nodes = nodes
+        self.E = E
+        self.A = A
+
+    def length(self):
+        ni, nj = self.get_nodes()
+        x0, x1, y0, y1 = ni.x, nj.x, ni.y, nj.y
+        _l = np.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
+        return _l
+
+    def theta(self):
+        ni, nj = self.get_nodes()
+        x0, x1, y0, y1 = ni.x, nj.x, ni.y, nj.y
+        theta = np.arctan2((y1 - y0), (x1 - x0))
+        return theta
+
+    def force(self):
+        return self._compute_force()
+
+    def stress(self):
+        s = self.force / self.A
+        return s
+
+    def _compute_force(self):
+        E, A, L = self.E, self.A, self.L
+        c = np.cos(self.theta)
+        s = np.sin(self.theta)
+        ni, nj = self.get_nodes()
+        u = np.array([ni.ux, ni.uy, nj.ux, nj.uy])
+        F = (E * A / L) * np.dot(np.array([-c, -s, c, s]), u)
+        return F
+
+    def get_element_stiffness(self):
+        c = np.cos(self.theta)
+        s = np.sin(self.theta)
+        cs = c * s
+        self._K = (self.E * self.A / self.L) * np.array([[c ** 2, cs, -c ** 2, -cs],
+                                                         [cs, s ** 2, -cs, -s ** 2],
+                                                         [-c ** 2, -cs, c ** 2, cs],
+                                                         [-cs, -s ** 2, cs, s ** 2]])
+
+    def get_nodes(self):
+        return self.nodes
+
