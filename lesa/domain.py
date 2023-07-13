1f4211b5d3bc404819b8c3b840e4c1a58f46b3e9
Konstantinos Mixios <k.mixios@gmail.com>
2023-07-13 10:51:06 +0300

    basic methods finished

--- lesa/domain.py
+++ lesa/domain.py
@@ -52,14 +52,56 @@ class Node:
         if elements == []:
             self._sx = 0.0
         else:
-            self._sx = sum([el.sx for el in elements])/len(elements)
+            self._sx = sum([el.sx for el in elements]) / len(elements)
         return self._sx
-    
+
     def sy(self):
         elements = self.con_elements
         if elements == []:
             self._sy = 0
         else:
-            self._sy = sum([el.sy for el in elements])/len(elements)
+            self._sy = sum([el.sy for el in elements]) / len(elements)
         return self._sy
+
+    def sxy(self):
+        elements = self.con_elements
+        if elements == []:
+            self._sxy = 0
+        else:
+            self._sxy = sum([el.sxy for el in elements]) / len(elements)
+        return self._sxy
+
+    def sqev(self):
+        sxx, syy, sxy = self.sx, self.sy, self.sxy
+
+        seqv = np.sqrt(sxx * 2 - sxx * syy + syy ** 2 + 3 * sxy * 2)
+        return seqv
+
+    def ex(self):
+        elements = self.con_elements
+        if elements == []:
+            self._ex = 0
+        else:
+            self._ex = sum([el.ex for el in elements]) / len(elements)
+        return self._ex
+
+    def ey(self):
+        elements = self.con_elements
+        if elements == []:
+            self._ey = 0
+        else:
+            self._ey = sum([el.ey for el in elements]) / len(elements)
+        return self._ey
+
+    def exy(self):
+        elements = self.con_elements
+        if elements == []:
+            self._exy = 0
+        else:
+            self._exy = sum([el.exy for el in elements])/len(elements)
+        return self._exy
+
+    def set_label(self, value):
+        self.label = f'node_{value}'
+
 
